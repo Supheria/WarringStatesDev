fd01fff51bac0139617fcf00f482d01c713c7ef8
Supheria <supheria@gmail.com>
2025-10-08 20:41:04 +0800

    添加地图单元格和地图网格，自动生成基础地图平面

---
+++ WarringStatesMap/Scenes/HexaGrid.cs
@@ -0,0 +1,49 @@
+using System;
+using Godot;
+
+namespace HexaMap;
+
+public partial class HexaGrid : Node3D
+{
+	[Export]
+	public int Width { get; set; } = 6;
+
+	[Export]
+	public int Height { get; set; } = 6;
+
+	[Export]
+	public PackedScene CellPrefeb { get; set; }
+
+	private HexaCell[] Cells { get; set; }
+
+	public override void _Ready()
+	{
+		Cells = new HexaCell[Width * Height];
+		var index = 0;
+		for (var x = 0; x < Width; x++)
+		{
+			for (var z = 0; z < Height; z++)
+			{
+				// TODO: Cells is useless here
+				var cell = Cells[index++] = CreateCell(x, z);
+				AddChild(cell);
+			}
+		}
+	}
+
+	public override void _Process(double delta) { }
+	
+	private HexaCell CreateCell(int x, int z)
+	{
+		var edgeLen = 2f; // TODO: use a member variant of the cell
+		var pos = new Vector3
+		{
+			X = x * edgeLen,
+			Y = 0f,
+			Z = z * edgeLen
+		};
+		var cell = CellPrefeb.Instantiate<HexaCell>();
+		cell.Position = pos;
+		return cell;
+	}
+}
