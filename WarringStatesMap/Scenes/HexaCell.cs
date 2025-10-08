8d71ceae0a17028a0f01917ed86f19ae50b8214f
Supheria <supheria@gmail.com>
2025-10-08 23:34:18 +0800

    减少调用GetNode()的次数

--- WarringStatesMap/Scenes/HexaCell.cs
+++ WarringStatesMap/Scenes/HexaCell.cs
@@ -5,15 +5,20 @@ namespace HexaMap;
 
 public partial class HexaCell : Node3D
 {
-	public void SetContent(string content)
-	{
-		var label = GetNode<Label3D>("Content");
-		label.Text = content;
-	}
+	private Label3D? Content { get; set; }
 
 	// Called when the node enters the scene tree for the first time.
-	public override void _Ready() { }
+	public override void _Ready()
+	{
+		Content = GetNode<Label3D>("Content");
+	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta) { }
+
+	public void SetContent(string content)
+	{
+		if (Content is not null)
+			Content.Text = content;
+	}
 }
