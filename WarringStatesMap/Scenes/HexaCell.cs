8d4c573a07d0ecad5d6c854407f224fbb4676bf2
Supheria <supheria@gmail.com>
2025-10-08 22:12:54 +0800

    添加单元格坐标，调整相机位置

--- WarringStatesMap/Scenes/HexaCell.cs
+++ WarringStatesMap/Scenes/HexaCell.cs
@@ -1,17 +1,19 @@
-using Godot;
 using System;
+using Godot;
 
 namespace HexaMap;
 
 public partial class HexaCell : Node3D
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+	public void SetContent(string content)
 	{
+		var label = GetNode<Label3D>("Content");
+		label.Text = content;
 	}
 
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready() { }
+
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+	public override void _Process(double delta) { }
 }
--- WarringStatesMap/Scenes/HexaGrid.cs
+++ WarringStatesMap/Scenes/HexaGrid.cs
@@ -12,38 +12,44 @@ public partial class HexaGrid : Node3D
 	public int Height { get; set; } = 6;
 
 	[Export]
-	public PackedScene CellPrefeb { get; set; }
+	public PackedScene CellPrefab { get; set; }
 
-	private HexaCell[] Cells { get; set; }
+	// private HexaCell[] Cells { get; set; }
 
 	public override void _Ready()
 	{
-		Cells = new HexaCell[Width * Height];
-		var index = 0;
+		// Cells = new HexaCell[Width * Height];
 		for (var x = 0; x < Width; x++)
 		{
 			for (var z = 0; z < Height; z++)
 			{
 				// TODO: Cells is useless here
-				var cell = Cells[index++] = CreateCell(x, z);
-				AddChild(cell);
+				CreateCell(x, z);
 			}
 		}
 	}
 
 	public override void _Process(double delta) { }
-	
-	private HexaCell CreateCell(int x, int z)
+
+	private void CreateCell(int x, int z)
 	{
 		var edgeLen = 2f; // TODO: use a member variant of the cell
 		var pos = new Vector3
 		{
 			X = x * edgeLen,
 			Y = 0f,
-			Z = z * edgeLen
+			Z = z * edgeLen,
 		};
-		var cell = CellPrefeb.Instantiate<HexaCell>();
+		var cell = CellPrefab.Instantiate<HexaCell>();
 		cell.Position = pos;
-		return cell;
+		cell.SetContent($"{x}\n{z}");
+		AddChild(cell);
+
+		// var label = new Label3D();
+		// label.Shaded = false;
+		// label.Text = $"{x}\n{z}";
+		// label.Position = pos;
+		// label.RotateX(float.DegreesToRadians(-90));
+		// AddChild(label);
 	}
 }
