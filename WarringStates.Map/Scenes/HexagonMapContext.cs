7d21e40c7c494d5945d3c078dc4144316b7cac28
Supheria <supheria@gmail.com>
2025-10-11 15:54:46 +0800

    测试并排除疑似_Input造成的内存泄漏
    参阅：
    
    https://github.com/godotengine/godot/issues/99347
    
    https://github.com/godotengine/godot/issues/82112

--- WarringStates.Map/Scenes/HexagonMapContext.cs
+++ WarringStates.Map/Scenes/HexagonMapContext.cs
@@ -5,6 +5,9 @@ public partial class HexagonMapContext : Node3D
 {
     public override void _Input(InputEvent @event)
     {
+        GC.Collect(GC.MaxGeneration, GCCollectionMode.Forced);
+        GC.WaitForPendingFinalizers();
+
         if (@event is not InputEventKey key || key.IsPressed())
             return;
         if (key.Keycode == Key.P)
