0e7b4759cdb12c36eea0024d180fb24caf63f4e6
Supheria <supheria@gmail.com>
2025-10-11 16:22:27 +0800

    删除GC测试
    详细内容查看：https://github.com/Supheria/WarringStatesDev/issues/3

--- WarringStates.Map/Scenes/HexagonMapContext.cs
+++ WarringStates.Map/Scenes/HexagonMapContext.cs
@@ -5,9 +5,6 @@ public partial class HexagonMapContext : Node3D
 {
     public override void _Input(InputEvent @event)
     {
-        GC.Collect(GC.MaxGeneration, GCCollectionMode.Forced);
-        GC.WaitForPendingFinalizers();
-
         if (@event is not InputEventKey key || key.IsPressed())
             return;
         if (key.Keycode == Key.P)
